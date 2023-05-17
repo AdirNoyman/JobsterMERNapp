057d5c8a8355e03a7584aa9b692d224403f82032
Adir Noyman <ofahol2007@hotmail.com>
2023-05-17 19:55:29 +0300

    Added test user middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,6 +46,7 @@ const login = async (req, res) => {
 // Update User
 const updateUser = async (req, res) => {
   const { email, name, lastName, location } = req.body;
+  // console.log(req.user);
   if (!email || !name || !lastName || !location) {
     throw new BadRequestError('Please provide all the necessary information');
   }
