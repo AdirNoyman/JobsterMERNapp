d4f1bb2b8805036e77b8b1415c8d0e8be864b153
Adir Noyman <ofahol2007@hotmail.com>
2023-05-20 19:16:54 +0300

    Added rate limiter

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const authenticateUser = require('../middleware/authentication');
 const testUser = require('../middleware/testUser');
+const rateLimiter = require('express-rate-limit');
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: { msg: 'Too many requests. Please try again later 🤨' },
+});
+
 const { register, login, updateUser } = require('../controllers/auth');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', apiLimiter, register);
+router.post('/login', apiLimiter, login);
 router.patch('/updateUser', authenticateUser, testUser, updateUser);
 
 module.exports = router;
